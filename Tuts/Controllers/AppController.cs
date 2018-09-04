383ec9b66c53ee844f9f1f82e3679c9c3df91568
Emre Karataşoğlu <emre.karatasoglu@maysiber.com>
2018-09-04 17:17:00 +0300

    First commit

---
+++ Tuts/Controllers/AppController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Tuts.Controllers
+{
+    public class AppController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet("app/contact")]
+        public IActionResult Contact()
+        {
+            
+            ViewBag.Title = "Contact";
+            return View();
+        }
+        [HttpPost("app/contact")]
+        public IActionResult Contact(object model)
+        {
+            return View();
+        }
+    }
+}
